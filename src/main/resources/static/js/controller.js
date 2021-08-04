1b387ebb7d04222d5c260f37224c4de4ee9e01cf
Osamu Hashimoto <hashimoto.osamu@gmail.com>
2021-08-04 13:46:37 +0900

    レンダリング時のレイアウトを修正。

--- src/main/resources/static/js/controller.js
+++ src/main/resources/static/js/controller.js
@@ -6,8 +6,44 @@ myApp.controller('UserController',
 		//alert("data = " + data);
 
 	    $scope.getUsersList = function() {
-			//alert("data = " + data);
+
+			var status = data.length;
+			var show = false;
+			var buf = null;
+			var newData = [];
+			alert("data.length = " + data.length);
+			for(var i = 0; i < status; i++) {
+				if (i % 3 == 2) {
+					show = true;
+				} else {
+					show = false;
+				}
+				
+				//newData[i] = { "alt": data[i].alt , "url": data[i].url, "show":  show };
+				newData[i] = { "alt": data[i].alt , "url": data[i].url , "show":  show };
+			}
+			
+			console.log(newData);
+			
+			data = JSON.stringify(newData);
+			//document.writeln(data);
+
+			data = JSON.parse(data);
+			console.log(data);
+
 			$scope.json = data;
 	    };
+
+		// undefined
+		//alert("$scope.index = " + $scope.index );
+		
+		// NaN
+		//alert("$scope.index % 3 =" +($scope.index % 3));
+
+		if($scope.index % 3 == 2) {
+			$scope.show = true;
+		} else {
+			$scope.show = false;
+		}
 	}
 );
