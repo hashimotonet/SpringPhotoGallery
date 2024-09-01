0d0b8000620bae9317212c12fb9a1696174db177
Osamu Hashimoto <hashimoto.osamu@gmail.com>
2024-09-01 11:18:31 +0900

    Update init_data.sql
    テーブル定義を更新する修正。

--- sql/init_data.sql
+++ sql/init_data.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.39, for Linux (x86_64)
 --
 -- Host: localhost    Database: gallery
 -- ------------------------------------------------------
--- Server version	8.0.32
+-- Server version       8.0.39
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -14,15 +14,79 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-DROP SCHEMA IF EXISTS gallery;
-CREATE SCHEMA gallery;
-USE gallery;
+mysqldump: Error: 'Access denied; you need (at least one of) the PROCESS privilege(s) for this operation' when trying to dump tablespaces
+
+--
+-- Table structure for table `SPRING_SESSION`
+--
+
+DROP TABLE IF EXISTS `SPRING_SESSION`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `SPRING_SESSION` (
+  `PRIMARY_ID` char(36) NOT NULL,
+  `SESSION_ID` char(36) NOT NULL,
+  `CREATION_TIME` bigint NOT NULL,
+  `LAST_ACCESS_TIME` bigint NOT NULL,
+  `MAX_INACTIVE_INTERVAL` int NOT NULL,
+  `EXPIRY_TIME` bigint NOT NULL,
+  `PRINCIPAL_NAME` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`PRIMARY_ID`),
+  UNIQUE KEY `SPRING_SESSION_IX1` (`SESSION_ID`),
+  KEY `SPRING_SESSION_IX2` (`EXPIRY_TIME`),
+  KEY `SPRING_SESSION_IX3` (`PRINCIPAL_NAME`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `SPRING_SESSION_ATTRIBUTES`
+--
+
+DROP TABLE IF EXISTS `SPRING_SESSION_ATTRIBUTES`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `SPRING_SESSION_ATTRIBUTES` (
+  `SESSION_PRIMARY_ID` char(36) NOT NULL,
+  `ATTRIBUTE_NAME` varchar(200) NOT NULL,
+  `ATTRIBUTE_BYTES` blob NOT NULL,
+  PRIMARY KEY (`SESSION_PRIMARY_ID`,`ATTRIBUTE_NAME`),
+  CONSTRAINT `SPRING_SESSION_ATTRIBUTES_FK` FOREIGN KEY (`SESSION_PRIMARY_ID`) REFERENCES `SPRING_SESSION` (`PRIMARY_ID`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `USER_ACCOUNT`
+--
+
+DROP TABLE IF EXISTS `USER_ACCOUNT`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `USER_ACCOUNT` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `ACCOUNT_ID` varchar(32) NOT NULL,
+  `EMAIL_ADDRESS` varchar(128) NOT NULL,
+  `PASSWORD` varchar(128) DEFAULT NULL,
+  `AUTHORITY` int NOT NULL DEFAULT '0',
+  `ACCOUNT_LOCK` tinyint(1) DEFAULT NULL,
+  `ACCOUNT_EXPIRATION_DATE` date DEFAULT NULL,
+  `PASSWORD_EXPIRATION_DATE` date DEFAULT NULL,
+  `ACCOUNT_GROUP` text,
+  `TELEPHONE1` varchar(15) DEFAULT NULL,
+  `TELEPHONE2` varchar(15) DEFAULT NULL,
+  `CREATED_AT` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `UPDATED_AT` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `ACCOUNT_ID` (`ACCOUNT_ID`),
+  UNIQUE KEY `EMAIL_ADDRESS` (`EMAIL_ADDRESS`),
+  KEY `ACCOUNT_ID_2` (`ACCOUNT_ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `account`
 --
 
-/*DROP TABLE IF EXISTS `account`;*/
+DROP TABLE IF EXISTS `account`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `account` (
@@ -35,24 +99,14 @@ CREATE TABLE `account` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `identity` (`identity`),
   KEY `identity_2` (`identity`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `account`
---
-
--- LOCK TABLES `gallery.account` WRITE;
-/*!40000 ALTER TABLE `account` DISABLE KEYS */;
--- INSERT INTO `gallery.account` VALUES (1,'hashimoto','osamu',1,'2023-08-10 03:09:59','2023-08-10 03:09:59');
-/*!40000 ALTER TABLE `account` ENABLE KEYS */;
--- UNLOCK TABLES;
 
 --
 -- Table structure for table `photo`
 --
 
-/*DROP TABLE IF EXISTS `photo`;*/
+DROP TABLE IF EXISTS `photo`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `photo` (
@@ -61,11 +115,22 @@ CREATE TABLE `photo` (
   `authority` int NOT NULL DEFAULT '0',
   `data` mediumblob NOT NULL,
   `thumbnail` mediumblob,
-  `alt` varchar(200) DEFAULT NULL,
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `alt` varchar(400) DEFAULT NULL,
+  `chat` text,
   PRIMARY KEY (`id`),
-  KEY `identity` (`identity`),
-  CONSTRAINT `photo_ibfk_1` FOREIGN KEY (`identity`) REFERENCES `account` (`identity`)
-) ENGINE=InnoDB AUTO_INCREMENT=99 DEFAULT CHARSET=utf8mb3;
+  KEY `identity` (`identity`)
+) ENGINE=InnoDB AUTO_INCREMENT=205 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-09-01 11:12:57
